4e3b067dfe3834ed84f9ec4120df2c55ccec4da8
Wouter Deconinck <wdconinc@gmail.com>
2020-06-18 19:43:48 -0500

    Add depends on hepmc2, and rely on hepmc3 being pulled in elsewhere

--- packages/escalate/package.py
+++ packages/escalate/package.py
@@ -16,10 +16,10 @@ class Escalate(BundlePackage):
     depends_on('geant4@10.6.1', when='@1.0.1')
     depends_on('eigen@3.3.7', when='@1.0.1')
     #depends_on('clhep@2.3.2.2', when='@1.0.1') # FIXME geant4@10.6.1 depends_on clhep@2.4.1.3
-    #depends_on('hepmc@2.06.09', when='@1.0.1') # FIXME unable to install both hepmc@2 and hepmc@3
-    depends_on('hepmc@3.2.1', when='@1.0.1')
     #depends_on('vgm@4.5', when='@1.0.1') # FIXME no package
     #depends_on('genfit@2020.1', when='@1.0.1') # FIXME no package
+    depends_on('hepmc@2.06.09', when='@1.0.1')
+    #depends_on('hepmc@3.2.1', when='@1.0.1') # FIXME Error: 2.06.09 does not satisfy 3.2.1
     depends_on('acts@0.22.0', when='@1.0.1')
     depends_on('delphes@3.4.2', when='@1.0.1')
     depends_on('fastjet@3.3.3', when='@1.0.1')
