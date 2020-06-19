136b82afae6570a5a3a1d54cefd2274ec148d7da
Wouter Deconinck <wdconinc@jlab.org>
2020-06-19 14:58:28 -0400

    Genfit cxxstd variant with depends_on root accordingly

--- packages/genfit/package.py
+++ packages/genfit/package.py
@@ -18,8 +18,21 @@ class Genfit(CMakePackage):
     version('2020-05-29', commit='36c46498d7cbdb6e1d25cdb7f7358c15433c03ff')
 
     #variant('rave', default=False, description="Include rave support") # FIXME
+    variant('cxxstd',
+            default='17',
+            values=('11', '14', '17'),
+            multi=False,
+            description='Use the specified C++ standard when building.')
 
     depends_on('cmake@2.8:', type='build')
-    depends_on('root@6.00.00:')
+    depends_on('root@6.00.00: cxxstd=11', when='cxxstd=11')
+    depends_on('root@6.00.00: cxxstd=14', when='cxxstd=14')
+    depends_on('root@6.00.00: cxxstd=17', when='cxxstd=17')
     depends_on('eigen')
 
+    def cmake_args(self):
+        args = []
+        # C++ Standard
+        args.append('-DCMAKE_CXX_STANDARD=%s'
+                    % self.spec.variants['cxxstd'].value)
+        return args
