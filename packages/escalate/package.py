968cff1a87c35858366861dcb070dd8deda768c1
Wouter Deconinck <wdconinc@jlab.org>
2020-06-20 11:22:38 -0400

    hepmc3 typo

--- packages/escalate/package.py
+++ packages/escalate/package.py
@@ -59,7 +59,7 @@ class Escalate(BundlePackage):
     depends_on('vgm@4.5', when='@1.0.1')
     depends_on('genfit@2020-06-03', when='@1.0.1')
     depends_on('hepmc@2.06.09', when='@1.0.1')
-    depends_on('hepmc@3.2.1', when='@1.0.1')
+    depends_on('hepmc3@3.2.1', when='@1.0.1')
     depends_on('acts@0.22.0', when='@1.0.1')
     depends_on('delphes@3.4.2', when='@1.0.1')
     depends_on('fastjet@3.3.3', when='@1.0.1')
