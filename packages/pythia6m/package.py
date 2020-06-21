e3b36dcc826126a8afbaba87ee889e883c2870ac
Wouter Deconinck <wdconinc@jlab.org>
2020-06-21 15:08:59 -0400

    New package pythia6m

---
+++ packages/pythia6m/package.py
@@ -0,0 +1,20 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Pythia6m(CMakePackage):
+    """Pythia6 modified to better describe lepton-nucleon scattering
+    at intermediate energies."""
+
+    homepage = "http://gitlab.com/eic/mceg/pythia6m"
+    url      = "http://gitlab.com/eic/mceg/pythia6m/-/archive/master/pythia6m-master.tar.gz"
+    git      = "http://gitlab.com/eic/mceg/pythia6m.git"
+
+    version('master', branch='master', submodules=True)
+
+    depends_on('cmake@2.8:', type='build')
+    depends_on('root')
