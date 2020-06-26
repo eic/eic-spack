a6d09a336144256de01951f1c51e7b2a5736fa57
Wouter Deconinck <wdconinc@jlab.org>
2020-06-26 12:54:07 -0400

    RapGap depends_on autotools, hepmc, lhapdf; fix paths; force_autoreconf

--- packages/rapgap/package.py
+++ packages/rapgap/package.py
@@ -20,10 +20,32 @@ class Rapgap(AutotoolsPackage):
     version('3.301', sha256='c85afafdebd4e2e0645dff3e2cca0371b2a6363487a468eb5213e6030edf9663')
     version('3.203', sha256='ac54fb78c78c3bb8ab8bc13139677d6daa155406c50acc74d05b4ec536b92e07')
 
+    depends_on('autoconf', type='build')
+    depends_on('automake', type='build')
+    depends_on('libtool',  type='build')
+    depends_on('m4',       type='build')
     depends_on('pythia6')
+    depends_on('lhapdf5')
+    depends_on('hepmc')
+
+    force_autoreconf = True
+
+    def patch(self):
+        # Search for libPythia, not libpythia
+        filter_file('libpythia6',
+                    'libPythia6',
+                    'configure.ac')
+        # Link libPythia6, not libpythia6
+        filter_file('-lpythia6 -lpythia6_dummy',
+                    '-lPythia6',
+                    'configure.ac')
 
     def configure_args(self):
         args = []
         args.append('--with-pythia6={0}'.format(
             self.spec['pythia6'].prefix))
+        args.append('--with-lhapdf={0}'.format(
+            self.spec['lhapdf5'].prefix))
+        args.append('--with-hepmc={0}'.format(
+            self.spec['hepmc'].prefix))
         return args
