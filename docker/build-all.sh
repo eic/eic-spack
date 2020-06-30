948e65bff48acdcd6c4c43dc91e70b7fce065f82
Wouter Deconinck <wdconinc@gmail.com>
2020-06-30 15:53:31 -0500

    New spack builders with travis CI

--- docker/build-all.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-dir=`dirname ${0}`
-dir=`realpath ${dir}`
-
-for os in `find ${dir} -mindepth 1 -type d` ; do
-  docker build -f ${os}/Dockerfile .
-done
---
+++ docker/entrypoint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+source /spack/share/spack/setup-env.sh
+exec "$@"
