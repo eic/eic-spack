18df9efa7f5ddfe744248220e58b9c1a6aa85338
Wouter Deconinck <wdconinc@gmail.com>
2020-07-01 15:01:33 -0500

    Docker and Singularity build with getopts to set custom repo
    This allows overriding the built-in eic-spack repository for testing
    of new packages in a local working copy, e.g.
    ```
    singularity-build.sh -o centos8 -r ~/git/eic-spack -m /data/spack/mirror eicroot
    ```

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -3,15 +3,47 @@
 dir=`dirname ${0}`
 dir=`realpath ${dir}`
 
-mkdir -p ${dir}/mirror
+os="centos7"
+repo=""
+mirror="${dir}/mirror"
+while getopts ":r:o:m:" opt; do
+  case ${opt} in
+    \? ) # process option h
+      echo "Usage: `basename $0` [ -o os ] [ -r repo ] [ -m mirror ] spec"
+      exit
+      ;;
+    o )
+      os=$OPTARG
+      echo "Using OS '${os}'"
+      ;;
+    r )
+      repo=`realpath $OPTARG`
+      echo "Using repo '${repo}'"
+      ;;
+    m )
+      mirror=`realpath $OPTARG`
+      echo "Using mirror '${mirror}'"
+      ;;
+    : )
+      echo "Invalid Option: -$OPTARG requires an argument" 1>&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND -1))
 
-os=$1
-shift
+mkdir -p ${mirror}
+
+binds="-v ${mirror}:/spack/mirror"
+if [ ! -z "${repo}" ] ; then
+  binds="${binds} -v ${repo}:/spack/var/spack/repos/eic-spack"
+fi
+
+container="electronioncollider/spack-builder:${os}"
 
 for spec in $@ ; do
-  docker run --rm \
-             -v ${dir}/mirror:/spack/mirror \
-             -it electronioncollider/spack-builder:${os} \
+  echo "Installing '${spec}'"
+  docker run --rm ${binds} -it ${container} \
     bash -c " \
       spack install --no-check-signature ${spec} && \
       spack buildcache create --rebuild-index -u -m local -r -a ${spec} \
--- docker/singularity-build.sh
+++ docker/singularity-build.sh
@@ -3,17 +3,49 @@
 dir=`dirname ${0}`
 dir=`realpath ${dir}`
 
-mkdir -p ${dir}/mirror
+os="centos7"
+repo=""
+mirror="${dir}/mirror"
+while getopts ":r:o:m:" opt; do
+  case ${opt} in
+    \? ) # process option h
+      echo "Usage: `basename $0` [ -o os ] [ -r repo ] [ -m mirror ] spec"
+      exit
+      ;;
+    o )
+      os=$OPTARG
+      echo "Using OS '${os}'"
+      ;;
+    r )
+      repo=`realpath $OPTARG`
+      echo "Using repo '${repo}'"
+      ;;
+    m )
+      mirror=`realpath $OPTARG`
+      echo "Using mirror '${mirror}'"
+      ;;
+    : )
+      echo "Invalid Option: -$OPTARG requires an argument" 1>&2
+      exit 1
+      ;;
+  esac
+done
+shift $((OPTIND -1))
 
-os=$1
-shift
+mkdir -p ${mirror}
+
+binds="-B ${mirror}:/spack/mirror"
+if [ ! -z "${repo}" ] ; then
+  binds="${binds} -B ${repo}:/spack/var/spack/repos/eic-spack"
+fi
+
+container="docker://electronioncollider/spack-builder:${os}"
 
 for spec in $@ ; do
-  singularity run \
-             -B ${dir}/mirror:/spack/mirror \
-             docker://electronioncollider/spack-builder:${os} \
+  echo "Installing '${spec}'"
+  singularity run ${binds} ${container} \
     bash -c " \
-      spack install ${spec} && \
-      spack buildcache create --rebuild-index -m local -a ${spec} \
+      spack install --no-check-signature ${spec} && \
+      spack buildcache create --rebuild-index -u -m local -r -a ${spec} \
     "
 done
