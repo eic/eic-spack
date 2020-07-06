df33c9e59ac3042aabef2ad802f6c6d210a72401
Wouter Deconinck <wdconinc@gmail.com>
2020-07-05 23:09:31 -0500

    New package bmf (BeAST Magnetic Field)

---
+++ packages/bmf/package.py
@@ -0,0 +1,18 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Bmf(CMakePackage):
+    """BeAST magnetic field map and a C++ class to handle it."""
+
+    homepage = "http://gihub.com/eic/BeastMagneticField"
+    url      = "http://gihub.com/eic/BeastMagneticField"
+    git      = "http://gihub.com/eic/BeastMagneticField.git"
+
+    version('master', branch='master')
+
+    depends_on('cmake@2.8.10', type='build')
