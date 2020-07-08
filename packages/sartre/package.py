fb7a131c03ba3f9576dcb6b68ec08a746ea8cae7
Wouter Deconinck <wdconinc@gmail.com>
2020-07-08 15:25:32 -0500

    Sartre variant hpmz sha256sum fixed, correct installation prefix

--- packages/sartre/package.py
+++ packages/sartre/package.py
@@ -4,6 +4,7 @@
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
 from spack import *
+import glob
 
 
 class Sartre(CMakePackage):
@@ -14,21 +15,23 @@ class Sartre(CMakePackage):
     homepage = "http://sartre.hepforge.org/"
     url      = "http://sartre.hepforge.org/downloads/?f=sartre-1.33-src.tgz"
 
-    variant('kmw',  default=True,  description='Install data tables with KMW parameters')
-    variant('hpmz', default=False, description='Install data tables with HPMZ parameters')
+    variant('kmw',  default=True,  description='Install data tables with KMW parameters (170 MB)')
+    variant('hpmz', default=False, description='Install data tables with HPMZ parameters (1.9 GB)')
 
     resource(
         name='tables-KMW.gz',
         url='https://rhig.physics.yale.edu/~ullrich/sartre-tables/tables-KMW.gz',
         sha256='0b8718766496c2b730b74a22ebb0053ffbc311d4a84a1f5ac8c755eeaf7e3ac7',
-        destination='tables',
+        destination='.',
+        extension='tar.gz',
         when='+kmw'
     )
     resource(
         name='tables-HPMZ.gz',
         url='https://rhig.physics.yale.edu/~ullrich/sartre-tables/tables-HPMZ.gz',
-        sha256='0b8718766496c2b730b74a22ebb0053ffbc311d4a84a1f5ac8c755eeaf7e3ac7', # FIXME
-        destination='tables',
+        sha256='8933853f37973bc7ec7448fcaa7c93b4491337eb4a0b28e8d3bd05aefc464e55',
+        destination='.',
+        extension='tar.gz',
         when='+hpmz'
     )
 
@@ -36,3 +39,14 @@ class Sartre(CMakePackage):
 
     depends_on('gsl')
     depends_on('root')
+
+    def patch(self):
+        # Collect all CMakeLists.txt
+        makefiles = glob.glob('**/CMakeLists.txt', recursive=True)
+        # Remove explicit sartre directory from install prefix
+        filter_file('DESTINATION sartre/',
+                    'DESTINATION ${CMAKE_INSTALL_PREFIX}/',
+                    *makefiles)
+        filter_file('DESTINATION \$\{CMAKE_INSTALL_PREFIX\}/sartre',
+                    'DESTINATION ${CMAKE_INSTALL_PREFIX}/',
+                    *makefiles)
