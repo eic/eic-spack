d3f68ef0026d13e2a187d655483a05b1ae068151
Wouter Deconinck <wdconinc@jlab.org>
2020-07-28 21:28:47 -0400

    Nightly build script for CI on the package recipes

---
+++ nightly-build.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Determine calling directory
+dir=`dirname $0`
+dir=`realpath $dir`
+echo "Using directory $dir for testing..."
+
+git reset --hard c7717f01e219dc51a63615fa2af40b39c8295501
+
+# Fetch updates but do not apply to working copy
+git fetch --all
+branch=`git rev-parse --abbrev-ref HEAD`
+packages=`git diff --name-only $branch origin/$branch | sed -n 's|packages/\([a-z0-9-]*\)/package.py|\1|p' | xargs`
+git pull
+
+# Mark updated packages dirty
+echo "Updated packages: ${packages}"
+for package in ${packages} ; do
+  if [ -d ${dir}/packages/${package}/.success ] ; then
+    rm ${dir}/packages/${package}/.success/*
+  else
+    mkdir -p ${dir}/packages/${package}/.success/
+  fi
+done
+
+# Test dirty packages
+for packagedir in ${dir}/packages/* ; do
+  package=`basename ${packagedir}`
+  versions=`sed -n 's|\s*version('\''\(.*\)'\'',.*$|\1|p' ${packagedir}/package.py | xargs`
+  echo "Checking ${package} @ ${versions}..."
+  for version in ${versions} ; do
+    if [ ! -f ${package}/.success/ ] ; then
+      export TINI_SUBREAPER=""
+      ${dir}/docker/singularity-build.sh -r ${dir} ${package}@${version} \
+          && touch ${package}/.success/${version}
+    fi
+  done
+done
