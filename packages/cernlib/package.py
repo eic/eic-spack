5c805b02580b0624374ef66fed237414a01bf804
Dmitry Romanov <romanovda@gmail.com>
2020-08-02 02:12:59 -0400

    New approach to cerbkub

---
+++ packages/cernlib/package.py
@@ -0,0 +1,90 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+# ----------------------------------------------------------------------------
+# If you submit this package back to Spack as a pull request,
+# please first remove this boilerplate and all FIXME comments.
+#
+# This is a template package file for Spack.  We've put "FIXME"
+# next to all the things you'll want to change. Once you've handled
+# them, you can save this file and test your package like this:
+#
+#     spack install cernlib
+#
+# You can edit this file again by typing:
+#
+#     spack edit cernlib
+#
+# See the Spack documentation for more information on packaging.
+# ----------------------------------------------------------------------------
+
+from spack import *
+import os
+
+class Cernlib(Package):
+    """The CERN Program Library is a large collection of general purpose libraries and modules """
+
+    homepage = "http://cernlib.web.cern.ch/cernlib/"
+    url      = "https://github.com/JeffersonLab/build_scripts/archive/2.0.tar.gz"
+
+    # FIXME: Add a list of GitHub accounts to
+    # notify when the package is updated.
+    # maintainers = ['github_user1', 'github_user2']
+
+    version('2.0',  sha256='83dc64f3db376ef23abd863f8f2a556f4c5c2a0ff522840d88014d6ec374a415')
+    version('1.59', sha256='6c68f89bd0cf684709931d9e7392cacd08d691399f5e2d3b04019aaef15ec879')
+
+    
+    depends_on('netlib-lapack')
+    depends_on('imake', when='build')
+
+    # Openstf has "Makefile" and "Makefile_gcc".
+    # "Makefile" is used only in Windows development environment.
+    # The build in Windows development environment is not confirmed.
+    def build(self, spec, prefix):
+        #import pdb; pdb.set_trace()
+        print("===============build===============")
+
+        # with working_dir('src'):
+        #     if '%gcc' in self.spec:
+        #         make('-f', 'Makefile_gcc')
+        #     elif '%fj' in self.spec:
+        #         make('-f', 'Makefile_gcc')
+        #     else:
+        #         make()
+
+    def install(self, spec, prefix):
+        print("install===")
+        #print(dir(spec))
+        
+        print(os.listdir(self.stage.source_path))
+        print(os.getcwd())
+        lapack_dir = os.path.join(prefix, 'lapack')
+        os.mkdir(lapack_dir)
+
+        with working_dir(lapack_dir):
+            print("lapack_dir!!!!!!!!!!!!!!!!")
+            print(os.getcwd())
+            bash("make", "-f", "jopa")
+            make("VERBOSE=1","-f",  self.stage.source_path + '/Makefile_lapack')
+
+        with working_dir(prefix):
+            print("HERE!!!!!!!!!!!!!!!!")
+            print(os.getcwd())
+            make("VERBOSE=1","-f",  self.stage.source_path + '/Makefile_cernlib_Vogt')
+        
+        #import pdb; pdb.set_trace()
+
+    #     # FIXME: Unknown build system
+    #     make()
+    #     make('install')
+
+
+    def setup_build_environment(self, env):
+        #print(dict(self.spec))
+        #env.set('LAPACK_HOME', os.path.join(self.spec['netlib-lapack'].prefix, 'lib'))
+        env.set('LAPACK_HOME', os.path.join(self.spec.prefix, 'lapack'))
+        env.set('BUILD_SCRIPTS', self.stage.source_path)
+        
