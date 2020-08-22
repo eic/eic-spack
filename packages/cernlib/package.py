0d4757c21f606a0c237b388d34338c3b25b904be
Wouter Deconinck <wdconinc@gmail.com>
2020-08-22 14:47:31 -0500

    [cernlib] remove boilerplate; flake8 fix

--- packages/cernlib/package.py
+++ packages/cernlib/package.py
@@ -3,25 +3,9 @@
 #
 # SPDX-License-Identifier: (Apache-2.0 OR MIT)
 
-# ----------------------------------------------------------------------------
-# If you submit this package back to Spack as a pull request,
-# please first remove this boilerplate and all FIXME comments.
-#
-# This is a template package file for Spack.  We've put "FIXME"
-# next to all the things you'll want to change. Once you've handled
-# them, you can save this file and test your package like this:
-#
-#     spack install cernlib
-#
-# You can edit this file again by typing:
-#
-#     spack edit cernlib
-#
-# See the Spack documentation for more information on packaging.
-# ----------------------------------------------------------------------------
-
 from spack import *
 import os
+
 
 class Cernlib(Package):
     """The CERN Program Library is a large collection of general purpose libraries and modules """
