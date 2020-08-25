d8f6da4b4aefcdf1919ff0e212a69b5244eba0e1
Wouter Deconinck <wdconinc@gmail.com>
2020-08-24 19:48:25 -0500

    [herwig] New package (ftfbs)

---
+++ packages/herwig/package.py
@@ -0,0 +1,50 @@
+# Copyright 2013-2020 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
+#
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
+from spack import *
+
+
+class Herwig(AutotoolsPackage):
+    """Herwig is a multi-purpose particle physics event generator."""
+
+    homepage = "https://herwig.hepforge.org"
+    url      = "https://herwig.hepforge.org/downloads/Herwig-7.1.0.tar.bz2"
+
+    maintainers = ['wdconinc']
+
+    version('7.1.0', sha256='333d11d5bc448dd1cd1f24f6945adc29203f2b235e18db655ed09e237dc78974')
+
+    variant('evtgen', default=False, description='Include evtgen event format support.')
+    variant('pythia8', default=False, description='Include Pythia8 event generator support.')
+
+    depends_on('thepeg')
+    depends_on('fastjet')
+    depends_on('gsl')
+    depends_on('boost')
+
+    depends_on('evtgen', when='+evtgen')
+    depends_on('pythia8', when='+pythia8')
+    # FIXME Optional dependencies:
+    #        --with-vbfnlo=$INSTALL_LOC
+    #        --with-njet=$INSTALL_LOC \
+    #        --with-gosam=$INSTALL_LOC
+    #        --with-openloops=/where/openloops/was/installed \
+    #        --with-madgraph=/where/madgraph/was/installed  \
+
+    @run_before('autoreconf')
+    def filter_gslcblas(self):
+        # This part is needed to avoid linking with gsl cblas
+        # interface which will mask the cblas interface
+        # provided by optimized libraries due to linking order
+        filter_file('-lgslcblas', '', 'configure.ac')
+
+    def configure_args(self):
+        args = []
+        args.append('--with-thepeg=' + self.spec['thepeg'].prefix)
+        if self.spec.satisfies('+evtgen'):
+            args.append('--with-evtgen=' + self.spec['evtgen'].prefix)
+        if self.spec.satisfies('+pythia8'):
+            args.append('--with-pythia=' + self.spec['pythia8'].prefix)
+        return args
