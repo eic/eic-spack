dfae0855376e623c327912a41e5ded5403bb30a7
Wouter Deconinck <wdconinc@gmail.com>
2020-09-11 13:09:02 -0500

    [herwig] new version 7.2.1, force autoreconf for gcc@10.0.0:

--- packages/herwig/package.py
+++ packages/herwig/package.py
@@ -14,10 +14,15 @@ class Herwig(AutotoolsPackage):
 
     maintainers = ['wdconinc']
 
-    version('7.1.0', sha256='333d11d5bc448dd1cd1f24f6945adc29203f2b235e18db655ed09e237dc78974')
+    version('7.2.1', sha256='d4fff32f21c5c08a4b2e563c476b079859c2c8e3b78d853a8a60da96d5eea686')
 
     variant('evtgen', default=False, description='Include evtgen event format support.')
     variant('pythia8', default=False, description='Include Pythia8 event generator support.')
+
+    depends_on('autoconf', type='build')
+    depends_on('automake', type='build')
+    depends_on('libtool',  type='build')
+    depends_on('m4',       type='build')
 
     depends_on('thepeg')
     depends_on('fastjet')
@@ -32,6 +37,14 @@ class Herwig(AutotoolsPackage):
     #        --with-gosam=$INSTALL_LOC
     #        --with-openloops=/where/openloops/was/installed \
     #        --with-madgraph=/where/madgraph/was/installed  \
+
+    force_autoreconf = True
+
+    def patch(self):
+        if self.spec.satisfies('%gcc@10.0.0:'):
+            filter_file('-ffixed-line-length-none',
+                        '-ffixed-line-length-none -fallow-argument-mismatch',
+                        'Looptools/Makefile.am')
 
     def configure_args(self):
         args = []
