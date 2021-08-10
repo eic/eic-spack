fc306cd4e56ea293a0faedeadab810aa7dfadc69
Wouter Deconinck <wdconinc@gmail.com>
2021-08-10 17:38:40 -0500

    [npdet] new version

--- packages/npdet/package.py
+++ packages/npdet/package.py
@@ -11,6 +11,8 @@ class Npdet(CMakePackage):
     maintainers = ['wdconinc']
 
     version('master', branch='master')
+    version('0.7.0', sha256='d842d5571960316e76530849fa03296dc270d90da48d557bf4bd2c358538eefe')
+    version('0.6.0', sha256='0b1adbb3aff5d8b8ef9c6e81ec63721bdf12f4c457465bfd584ddeba63161edd')
     version('0.5.0', sha256='2ff4cd7992b7c18c25da64aa2d6223c210ea50c5ce90bcb007c0346cb4aee2c5')
 
     variant('build_type', default='Release',
