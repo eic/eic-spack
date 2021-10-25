38c0d86a06b021a542ff72e6d1936ab3c8be887d
Wouter Deconinck <wdconinc@gmail.com>
2021-10-25 14:45:34 -0500

    [fluka] new version 4.2.0

--- packages/fluka/package.py
+++ packages/fluka/package.py
@@ -16,28 +16,19 @@ class Fluka(Package):
        https://spack.readthedocs.io/en/latest/mirrors.html"""
 
     homepage = "https://fluka.cern"
-    url      = "https://flukafiles.web.cern.ch/flukafiles/fluka-4-1.1/fluka-4-1.1.Linux-gfor9.tgz"
     list_url = "https://fluka.cern/download/latest-fluka-release"
 
     maintainers = ['wdconinc']
 
     tags = ['eic']
 
-    version('4.1.1',
-            sha256='68cc4b81c04fe2d37f4e7ebeb0f8dfd452e1d8558cae8a611a9e1d7a93613f71',
-            url='file://{0}/fluka-4-1.1.Linux-gfor9.tgz'.format(os.getcwd()))
+    version('4.2.0',
+            sha256='a3b3f9617079aa64d39632b32a01b65dc32375d01804bb80ae1d3e87de599393',
+            url='file://{}/fluka-4-2.0.x86-Linux-gfor9.tgz'.format(os.getcwd()))
 
-    conflicts('%gcc@:7', when='@4.1.1')
-
-    gfor_ver = {
-        '4.1.1': '9',
-    }
+    conflicts('%gcc@:7', when='@4.2.0')
 
     manual_download = True
-
-    def url_for_version(self, version):
-        url = "https://flukafiles.web.cern.ch/flukafiles/fluka-{0}-{1}.fluka-{0}-{1}.Linux-gfor{2}.tgz"
-        return url.format(version[0], version[1:], gfor_ver[version])
 
     def install(self, spec, prefix):
         with working_dir('src'):
