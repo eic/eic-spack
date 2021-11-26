7c562a8ff082b0630762aab08c90f65f5e7965a4
Wouter Deconinck <wdconinc@gmail.com>
2021-11-26 13:46:51 -0600

    athena-ip6@1.0.0, juggler@4.3.0, npdet@1.2.0

--- packages/athena-ip6/package.py
+++ packages/athena-ip6/package.py
@@ -14,6 +14,7 @@ class AthenaIp6(CMakePackage):
     tags = ['eic']
 
     version('master', branch='master')
+    version('1.0.0', sha256='f387f049e8b4c25238dcaca29965c74445f83979ff56de44ef3b8df8d0893723')
     version('0.6.2', sha256='c5daf21952823356aacadc183a71a1888bda6734f48938c61a7c54ae73e40231')
     version('0.6.1', sha256='df111e1596f65f0d43555ea4d7a31e21199be34a68e481f884350a22792ab433')
     version('0.6.0', sha256='d44889f446aab78b28f9851d95e7ce1105f89c6034513b96c6cd7e3e50a03289')
