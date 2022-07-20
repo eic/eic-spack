25c44380140fd22b280c17d9b80fdea0aaf5be73
Wouter Deconinck <wdconinc@gmail.com>
2022-07-20 10:59:13 +0200

    jana2: new version 2.0.5

--- packages/jana2/package.py
+++ packages/jana2/package.py
@@ -18,6 +18,7 @@ class Jana2(CMakePackage):
 
     tags = ['eic']
 
+    version('2.0.5', sha256='2e7297dfb0bd7f4a2f2fa3bca6b1c10b2553d321dec6060e48b0d75a5ed6717d')
     version('2.0.4', sha256='848adffcb881beb7835d01ce47a58991bb4f92664c9477196960ce8cfd94a3ca')
     version('2.0.3', sha256='fd34c40e2d6660ec08aca9208999dd9c8fe17de21c144ac68b6211070463e415')
     version('2.0.2', sha256='161d29c2b1efbfb36ec783734b45dff178b0c6bd77a2044d5a8829ba5b389b14')
