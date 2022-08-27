e5bef7d72c1e41c126b0416770391f1fe8ac2818
Wouter Deconinck <wdconinc@gmail.com>
2022-08-27 10:48:56 -0500

    feat: add patch for Geant4OpticalTrackerAction

--- packages/dd4hep/package.py
+++ packages/dd4hep/package.py
@@ -4,3 +4,8 @@ from spack.pkg.builtin.dd4hep import Dd4hep as BuiltinDd4hep
 
 class Dd4hep(BuiltinDd4hep):
     version("1.21", sha256="0f9fe9784bf28fa20ce5555ff074430da430e9becc2566fe11e27c4904a51c94")
+    patch(
+        "https://github.com/AIDASoft/DD4hep/commit/8693a29669d03dec5e06b61e6df7cc0df1e0aa5c.patch",
+        sha256="28fb1c17eb1c06c24b304511308fd3b0af708f2ba3aec3e4cb13d7da6abbc51c",
+        when="@1.21:",
+    )
