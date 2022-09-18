3fd95ec6473a88e59766bc512daaa5595d0c5fb2
Wouter Deconinck <wdconinc@gmail.com>
2022-09-18 18:19:49 -0500

    geant4: new version 11.0.3 (backport to v0.18)

--- packages/geant4/package.py
+++ packages/geant4/package.py
@@ -3,5 +3,7 @@ from spack.pkg.builtin.geant4 import Geant4 as BuiltinGeant4
 
 
 class Geant4(BuiltinGeant4):
+    version("11.0.3", sha256="1e6560b802aa84e17255b83987dfc98a1457154fb603d0f340fae978238de3e7")
+
     ## Add to the hardcoded GEANT4 Birk's constants
     patch('birks.patch', when='@10.7.0:')
