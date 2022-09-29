23074dfbaeaeaaaf8cff49fb2f9af6a07d1859ba
Wouter Deconinck <wdconinc@gmail.com>
2022-09-29 08:53:28 -0500

    dd4hep: add v1.22 (backports to v0..18)

--- packages/dd4hep/package.py
+++ packages/dd4hep/package.py
@@ -3,6 +3,7 @@ from spack.pkg.builtin.dd4hep import Dd4hep as BuiltinDd4hep
 
 
 class Dd4hep(BuiltinDd4hep):
+    version("1.22", sha256="0e729b8897b7a9c348bc3304c63d4efd1a88e032a2ff5a8c4daf6c927fd7f8ee")
     version("1.21", sha256="0f9fe9784bf28fa20ce5555ff074430da430e9becc2566fe11e27c4904a51c94")
     patch(
         "https://github.com/AIDASoft/DD4hep/commit/8693a29669d03dec5e06b61e6df7cc0df1e0aa5c.patch",
