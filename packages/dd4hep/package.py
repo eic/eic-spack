f15cda49e83c3ff37a3e22fcc5bf93b4bfec4222
Wouter Deconinck <wdconinc@gmail.com>
2022-10-03 15:34:30 -0500

    dd4hep: end range of patch now included upstream

--- packages/dd4hep/package.py
+++ packages/dd4hep/package.py
@@ -9,5 +9,5 @@ class Dd4hep(BuiltinDd4hep):
     patch(
         "https://github.com/AIDASoft/DD4hep/commit/8693a29669d03dec5e06b61e6df7cc0df1e0aa5c.patch",
         sha256="28fb1c17eb1c06c24b304511308fd3b0af708f2ba3aec3e4cb13d7da6abbc51c",
-        when="@1.21:",
+        when="@1.21:1.22",
     )
