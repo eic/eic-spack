d6c1d5232d5909eeb3c44be98d9a33ca644f313b
Wouter Deconinck <wdconinc@gmail.com>
2022-10-05 14:54:12 -0500

    EICrecon 0.1.0 backport to v0.18 (new) (#264)
    * EICrecon: remove package
    
    This package is still too much in flux with no clarity on dependencies or any versions. We cannot pretend it works.
    
    * feat: updated to current standard (still no version)
    
    * eicrecon: new version 0.1.0 (#262)

--- packages/eicrecon/package.py
+++ packages/eicrecon/package.py
@@ -19,22 +19,13 @@ class Eicrecon(CMakePackage):
     version('main', branch='main')
     version("0.1.0", sha256="dcc8b60530a627c825413c07472659ba155600339ef8b8e742e3c997bcc504ae")
 
-    depends_on('cmake@3.8:', type='build')
-    depends_on('jana2')
-    depends_on('dd4hep')
+    depends_on('cmake@3.16:', type='build')
+
+    depends_on('jana2 +root +zmq')
+    depends_on('dd4hep +ddrec +edm4hep')
+    depends_on('edm4eic')
     depends_on('edm4hep')
-    depends_on('eic-ip6')
-    depends_on('epic-eic')
+    depends_on('podio')
+    depends_on('acts +dd4hep +edm4hep +identification +tgeo')
+    depends_on('root')
     depends_on('fmt')
-
-    def setup_build_environment(self, env):
-        env.set('EDM4HEP_ROOT', self.spec['edm4hep'].prefix)
-
-    def cmake_args(self):
-        args = [
-            self.define('DETECTOR_LIBS_IP6',
-                self.spec['eic-ip6'].prefix),
-            self.define('DETECTOR_LIBS_EPIC',
-                self.spec['epic-eic'].prefix),
-        ]
-        return args
