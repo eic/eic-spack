6c194d2db5c6c5eb6cb381e285934072e9425b31
Wouter Deconinck <wdconinc@gmail.com>
2022-10-05 12:32:56 -0500

    eicrecon: new version 0.1.0 (#263)

--- packages/eicrecon/package.py
+++ packages/eicrecon/package.py
@@ -10,12 +10,14 @@ class Eicrecon(CMakePackage):
     """EIC Reconstruction - JANA based."""
 
     homepage = "https://github.com/eic/eicrecon"
-    url      = "https://github.com/eic/eicrecon"
+    url      = "https://github.com/eic/EICrecon/archive/refs/tags/v0.1.0.zip"
     git      = "https://github.com/eic/eicrecon.git"
+    list_url = "https://github.com/eic/EICrecon/releases"
 
     maintainers = ['wdconinc']
 
     version('main', branch='main')
+    version("0.1.0", sha256="dcc8b60530a627c825413c07472659ba155600339ef8b8e742e3c997bcc504ae")
 
     depends_on('cmake@3.8:', type='build')
     depends_on('jana2')
