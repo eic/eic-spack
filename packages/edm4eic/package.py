d21bc3949e89a4b07cf4c250ca4e09c260bf1c64
Wouter Deconinck <wdconinc@gmail.com>
2022-10-16 11:35:57 -0500

    edm4eic: new version 1.2.0 (#271)
    ### Briefly, what does this PR introduce?
    New version for EDM4eic: no build system changes.
    
    ### What kind of change does this PR introduce?
    - [ ] Bug fix (issue #__)
    - [X] New feature (issue #__)
    - [ ] Documentation update
    - [ ] Other: __
    
    ### Please check if this PR fulfills the following:
    - [ ] Tests for the changes have been added
    - [ ] Documentation has been added / updated
    - [ ] Changes have been communicated to collaborators
    
    ### Does this PR introduce breaking changes? What changes might users
    need to make to their code?
    No.
    
    ### Does this PR change default behavior?
    No.

--- packages/edm4eic/package.py
+++ packages/edm4eic/package.py
@@ -14,6 +14,7 @@ class Edm4eic(CMakePackage):
     tags = ['eic']
 
     version('main', branch='main')
+    version("1.2.0", sha256="e70ec6d2a93002237c1bfd0046e96f3838f9dab3f5326bdb17826999b5f42759")
     version("1.1.0", sha256="f50a6ef77d8247aa30da5b1e574bb24ab82c86c8706a8f3900ff151dafe9a754")
     version("1.0.1", sha256="683dcd463757f9e4ad47e493be1f5fb40a6c1aae7d249ff18a19367384a61070")
     version("1.0.0", sha256="700ae7453f16786db4d3ace1a146914e1f0b935a08039c9f1f6a5ebe4aa173ae")
