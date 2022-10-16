7380d229032574e2feb7b2b9a0c125d7ac57611f
Wouter Deconinck <wdconinc@gmail.com>
2022-10-16 11:36:13 -0500

    eicrecon: new versions 0.2.0, 0.2.1 (#272)
    ### Briefly, what does this PR introduce?
    New versions of eicrecon, 0.2.0 and 0.2.1
    
    ### What kind of change does this PR introduce?
    - [ ] Bug fix (issue #__)
    - [X] New feature (issue #__)
    - [ ] Documentation update
    - [ ] Other: __
    
    ### Please check if this PR fulfills the following:
    - [ ] Tests for the changes have been added
    - [ ] Documentation has been added / updated
    - [ ] Changes have been communicated to collaborators
    
    ### Does this PR introduce breaking changes? What changes might users
    need to make to their code?
    No.
    
    ### Does this PR change default behavior?
    No.

--- packages/eicrecon/package.py
+++ packages/eicrecon/package.py
@@ -10,23 +10,34 @@ class Eicrecon(CMakePackage):
     """EIC Reconstruction - JANA based."""
 
     homepage = "https://github.com/eic/eicrecon"
-    url      = "https://github.com/eic/EICrecon/archive/refs/tags/v0.1.0.zip"
-    git      = "https://github.com/eic/eicrecon.git"
-    list_url = "https://github.com/eic/EICrecon/releases"
+    url = "https://github.com/eic/EICrecon/archive/refs/tags/v0.1.0.zip"
+    git = "https://github.com/eic/eicrecon.git"
+    list_url = "https://github.com/eic/EICrecon/tags"
 
-    maintainers = ['wdconinc']
+    maintainers = ["wdconinc"]
 
-    version('main', branch='main')
-    version("0.1.0", sha256="dcc8b60530a627c825413c07472659ba155600339ef8b8e742e3c997bcc504ae")
+    version("main", branch="main")
+    version(
+        "0.2.1",
+        sha256="097fef82cacd45453770f30e7e0ae382a11660b8bd4dfe478e7488a8988b8816",
+    )
+    version(
+        "0.2.0",
+        sha256="3fc0b812637d6bca9587cb4dadcd4b2ca386458ff6d46551ed8cf291335b4780",
+    )
+    version(
+        "0.1.0",
+        sha256="dcc8b60530a627c825413c07472659ba155600339ef8b8e742e3c997bcc504ae",
+    )
 
-    depends_on('cmake@3.16:', type='build')
+    depends_on("cmake@3.16:", type="build")
 
-    depends_on('jana2 +root +zmq')
-    depends_on('dd4hep +ddrec +edm4hep')
-    depends_on('edm4eic')
-    depends_on('edm4hep')
-    depends_on('podio')
-    depends_on('acts +dd4hep +identification +tgeo')
-    depends_on('root')
-    depends_on('fmt')
+    depends_on("jana2 +root +zmq")
+    depends_on("dd4hep +ddrec +edm4hep")
+    depends_on("edm4eic")
+    depends_on("edm4hep")
+    depends_on("podio")
+    depends_on("acts +dd4hep +identification +tgeo")
+    depends_on("root")
+    depends_on("fmt")
     depends_on("spdlog")
