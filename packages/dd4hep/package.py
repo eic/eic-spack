d31aa9e9687810be4a087444c97b6d820cd31880
Wouter Deconinck <wdconinc@gmail.com>
2022-11-20 13:09:08 -0600

    dd4hep: two patches to support hepmc3 files on xrootd servers (#320)
    ### Briefly, what does this PR introduce?
    This adds support for `.hepmc3` and `.hepmc3.tree.root` extensions in
    ddsim.

--- packages/dd4hep/package.py
+++ packages/dd4hep/package.py
@@ -13,3 +13,13 @@ class Dd4hep(BuiltinDd4hep):
         sha256="969fbdd9a35a07fe91d6376517621d3ddba28f13668d139fd9405052e3e6f1a6",
         when="@:1.23",
     )
+    patch(
+        "https://github.com/AIDASoft/DD4hep/pull/989.patch?full_index=1",
+        sha256="b29bec9faac4461f799f0ed12b85bf929ae4126fbf591fa8e2cca51fffae12e7",
+        when="@1.23",
+    )
+    patch(
+        "https://github.com/AIDASoft/DD4hep/pull/1011.patch?full_index=1",
+        sha256="15f4b9cc6e36aea836191b2154c0609a1ab55f085a0836609dfca804cbe78c6d",
+        when="@1.23",
+    )
