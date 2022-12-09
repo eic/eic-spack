d2da7f617816222c697775193d5be9b878947feb
Wouter Deconinck <wdconinc@gmail.com>
2022-12-09 16:28:00 -0600

    eicrecon: 0.3.6.1, 0.3.7 (#333)
    ### Briefly, what does this PR introduce?
    New versions 0.3.6.1, 0.3.7

--- packages/eicrecon/package.py
+++ packages/eicrecon/package.py
@@ -18,6 +18,14 @@ class Eicrecon(CMakePackage):
 
     version("main", branch="main")
     version(
+        "0.3.7",
+        sha256="6499661af54703b280410b4bd71324aea756379fca269eaed53f047d1acb0475",
+    )
+    version(
+        "0.3.6.1",
+        sha256="bc38b1b354dd9fbe97e98ddd4bc677aee6173121240df77ade8db1f680eb82a4",
+    )
+    version(
         "0.3.6",
         sha256="dac1ad140d0f9cd7d86e7df3fb2160f430b7971b20339281658b86c4450321d9",
     )
