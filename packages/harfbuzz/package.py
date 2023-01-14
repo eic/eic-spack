19d0439d2b2867f004ceee81a8d3fd07cd7c959a
Wouter Deconinck <wdconinc@gmail.com>
2023-01-13 23:49:40 -0600

    harfbuzz: line feed issues (#364)
    ### Briefly, what does this PR introduce?
    Line feed issues

--- packages/harfbuzz/package.py
+++ packages/harfbuzz/package.py
@@ -5,4 +5,7 @@ from spack.pkg.builtin.harfbuzz import Harfbuzz as BuiltinHarfbuzz
 class Harfbuzz(BuiltinHarfbuzz):
     version("5.3.1", sha256="4a6ce097b75a8121facc4ba83b5b083bfec657f45b003cd5a3424f2ae6b4434d")
 
-    def setup_dependent_build_environment(self, env, dependent_spec):                                                                           env.prepend_path("XDG_DATA_DIRS", self.prefix.share)                env.prepend_path("GI_TYPELIB_PATH", join_path(self.prefix.lib, "girepository-1.0"))
+    def setup_dependent_build_environment(self, env, dependent_spec):
+        env.prepend_path("XDG_DATA_DIRS", self.prefix.share)
+        env.prepend_path("GI_TYPELIB_PATH", join_path(self.prefix.lib, "girepository-1.0"))
+
