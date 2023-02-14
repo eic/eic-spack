844a2416d080d893b8f7aaee6a20e5ebe2e10d2b
Wouter Deconinck <wdconinc@gmail.com>
2023-02-14 09:19:55 -0600

    py-minkowskiengine: rm since base not in v0.19

--- packages/py-minkowskiengine/package.py
+++
@@ -1,7 +0,0 @@
-from spack import *
-from spack.pkg.builtin.py_minkowskiengine import PyMinkowskiengine as BuiltinPyMinkowskiengine
-
-
-class PyMinkowskiengine(BuiltinPyMinkowskiengine):
-    git = "https://github.com/NVIDIA/MinkowskiEngine"
-    version("master", branch="master")
