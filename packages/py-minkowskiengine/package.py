f892e2b5d7ea9d1f2e43741499e899ce21dd3d5a
Wouter Deconinck <wdconinc@gmail.com>
2023-02-13 23:56:20 -0600

    py-minkowskiengine: new package (#390)
    ### Briefly, what does this PR introduce?
    py-minkowskiengine git overlay

---
+++ packages/py-minkowskiengine/package.py
@@ -0,0 +1,7 @@
+from spack import *
+from spack.pkg.builtin.py_minkowskiengine import PyMinkowskiengine as BuiltinPyMinkowskiengine
+
+
+class PyMinkowskiengine(BuiltinPyMinkowskiengine):
+    git = "https://github.com/NVIDIA/MinkowskiEngine"
+    version("master", branch="master")
