2a2ac5598755ddffdbf583f27e7563835696525f
Wouter Deconinck <wdconinc@gmail.com>
2023-05-03 16:21:03 -0500

    dd4hep: B-field fix patch

--- packages/dd4hep/package.py
+++ packages/dd4hep/package.py
@@ -41,6 +41,11 @@ class Dd4hep(BuiltinDd4hep):
         when="@1.25.1",
     )
     patch(
+        "https://github.com/AIDASoft/DD4hep/pull/1080.patch?full_index=1",
+        sha256="2dde47795f8534fcbfb9454b3b729a5a758e2dc90b6bd9f5f8bdc8940e2da0f7",
+        when="@1.25.1",
+    )
+    patch(
         "https://github.com/AIDASoft/DD4hep/commit/8693a29669d03dec5e06b61e6df7cc0df1e0aa5c.patch",
         sha256="28fb1c17eb1c06c24b304511308fd3b0af708f2ba3aec3e4cb13d7da6abbc51c",
         when="@1.21:1.22",
