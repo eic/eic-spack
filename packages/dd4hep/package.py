4601858f2239905516c60ab5ec6201f267f9a593
Wouter Deconinck <wdconinc@gmail.com>
2023-05-04 21:20:45 -0500

    Update package.py

--- packages/dd4hep/package.py
+++ packages/dd4hep/package.py
@@ -31,7 +31,7 @@ class Dd4hep(BuiltinDd4hep):
     )
     patch(
         "pr1106.1.25.1.patch",
-        sha256="73932633e95272f77c1f6ad8edf06d426b198c30a9c9467167c04f3a3f7f5d12",
+        sha256="a69361435660a0c5134a93eb22c9021d122667772c9994642abb41587fdfdb5d",
         when="@1.25.1",
     )
     patch(
