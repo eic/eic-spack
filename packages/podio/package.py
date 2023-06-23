17b7e51bf03892aa8c88823ccdde5fa2ad81e887
Wouter Deconinck <wdconinc@gmail.com>
2023-06-23 11:14:44 -0500

    podio: backport PR434 (#473)
    Backport the operational commit in
    https://github.com/AIDASoft/podio/pull/434 into v0.16.4 and v0.16.5

--- packages/podio/package.py
+++ packages/podio/package.py
@@ -16,3 +16,8 @@ class Podio(BuiltinPodio):
         sha256="a6651073f633c937450731129ce4f45428174a70f0b2328c200b56ce42872b65",
         when="@0.16.1:0.16.2",
     )
+    patch(
+        "https://github.com/AIDASoft/podio/pull/434/commits/9f9c5fc8d40bc3e037b0846d5133d9a0e4d15c36.patch?full_index=1",
+        sha256="591aa122f55042b089303a4411418e2ac253469a8b6f6a79ebd14868fb88c1e5",
+        when="@0.16.4:0.16.5",
+    )
