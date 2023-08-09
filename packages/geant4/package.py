2598fc0358cf741ce78c53325df7f2df6cd39be7
Wouter Deconinck <wdconinc@gmail.com>
2023-08-09 15:12:10 -0500

    geant4: add versions with backported eAST physics list
    (cherry picked from commit 8f9a3c55f641cd3fefcfabb030bc7fe807e525ee)

--- packages/geant4/package.py
+++ packages/geant4/package.py
@@ -3,5 +3,8 @@ from spack.pkg.builtin.geant4 import Geant4 as BuiltinGeant4
 
 
 class Geant4(BuiltinGeant4):
+    ## Versions with the eAST physics list
+    version("11.1.1.east", git="https://github.com/eic/geant4", branch="east-v11.1.1")
+
     ## Add to the hardcoded GEANT4 Birk's constants
     patch("birks.patch", when="@10.7.0:")
