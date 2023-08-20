91709b1fe381c2acef91cb7575dba2e375434d1c
Wouter Deconinck <wdconinc@gmail.com>
2023-08-20 09:24:54 -0500

    epic-eic: fix typo

--- packages/epic-eic/package.py
+++ packages/epic-eic/package.py
@@ -70,7 +70,7 @@ class EpicEic(CMakePackage):
     depends_on("dd4hep@1.21: +ddg4 +ddrec", when="@:23.03.0")
     depends_on("dd4hep@1.21: +ddrec", when="@23.05.0:")
 
-    depends_on("acts-dd4hep", when=@:23.01.0")
+    depends_on("acts-dd4hep", when="@:23.01.0")
     
     depends_on("fmt +shared")
     depends_on("py-pyyaml")
