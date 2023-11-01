6c647882525b9fd15d10e45bd61668f9c983189a
Wouter Deconinck <wdconinc@gmail.com>
2023-11-01 16:20:25 -0500

    fix: ignore beta; open pull request (#570)
    ### Briefly, what does this PR introduce?
    Ignore beta versions, automatically open pull request instead of issue.

--- .github/scripts/checksum_new_versions.sh
+++ .github/scripts/checksum_new_versions.sh
@@ -8,7 +8,7 @@ package_list=$(spack tags eic)
 for p in ${package_list}; do
   v=$(spack versions --new $p)
   # ignore pre and rc versions (for all packages)
-  v=$(echo $v | sed 's/\S*\(rc\|pre\|alpha\)\S*//g')
+  v=$(echo $v | sed 's/\S*\(rc\|pre\|alpha\|beta\)\S*//g')
   # using `echo $v` instead of "$v" will handle v=" " correctly
   if [[ ! -z `echo $v` ]]; then
     echo "- [ ] \`$p\`: \`$v\` " >> gh-new-version.log
