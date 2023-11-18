a9b86f306da3fa22c4ae6a15f8e9bca222dc831d
Wouter Deconinck <wdconinc@gmail.com>
2023-11-18 16:53:33 -0600

    geant4: new version 11.1.3.east
    (cherry picked from commit 930938e96d6cbb83e109438a2ec2110822d47e50)

--- packages/geant4/package.py
+++ packages/geant4/package.py
@@ -4,6 +4,7 @@ from spack.pkg.builtin.geant4 import Geant4 as BuiltinGeant4
 
 class Geant4(BuiltinGeant4):
     ## Versions with the eAST physics list
+    version("11.1.3.east", git="https://github.com/eic/geant4", branch="east-v11.1.3")
     version("11.1.2.east", git="https://github.com/eic/geant4", branch="east-v11.1.2")
     version("11.1.1.east", git="https://github.com/eic/geant4", branch="east-v11.1.1")
 
