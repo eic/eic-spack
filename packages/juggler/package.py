6a02131df0c847445d49740a91febe21147500d1
Wouter Deconinck <wdconinc@gmail.com>
2023-11-19 22:55:26 -0600

    juggler: main, not master
    (cherry picked from commit 6bb62059b11db3394aedbbd89baaafc900e548cd)

--- packages/juggler/package.py
+++ packages/juggler/package.py
@@ -154,7 +154,7 @@ class Juggler(CMakePackage):
     depends_on("gaudi@33:34", when="@:1.8")
 
     depends_on("acts +identification +json +tgeo +dd4hep")
-    depends_on("acts", when="@master")
+    depends_on("acts", when="@main")
     depends_on("acts@30:", when="@11:")
     depends_on("acts@20.2:21", when="@9.1:10")
     depends_on("acts@19.9:19", when="@9.0")
