d4832a803cdd75224a5565474d14003fd14c482e
Wouter Deconinck <wdconinc@gmail.com>
2024-02-23 19:30:02 -0600

    geant4: rm birks.patch

--- packages/geant4/package.py
+++ packages/geant4/package.py
@@ -7,6 +7,3 @@ class Geant4(BuiltinGeant4):
     version("11.1.3.east", git="https://github.com/eic/geant4", branch="east-v11.1.3")
     version("11.1.2.east", git="https://github.com/eic/geant4", branch="east-v11.1.2")
     version("11.1.1.east", git="https://github.com/eic/geant4", branch="east-v11.1.1")
-
-    ## Add to the hardcoded GEANT4 Birk's constants
-    patch("birks.patch", when="@10.7.0:")
