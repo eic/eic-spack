93ab0a6db8ca306e79637409dc40e8e72d46155a
Wouter Deconinck <wdconinc@gmail.com>
2024-03-07 15:19:33 -0600

    geant4: apply patch in package.py

--- packages/geant4/package.py
+++ packages/geant4/package.py
@@ -9,3 +9,5 @@ class Geant4(BuiltinGeant4):
     version("11.1.3.east", git="https://github.com/eic/geant4", branch="east-v11.1.3")
     version("11.1.2.east", git="https://github.com/eic/geant4", branch="east-v11.1.2")
     version("11.1.1.east", git="https://github.com/eic/geant4", branch="east-v11.1.1")
+
+    patch("G4LogicalSkinSurface.patch", when="@11.2")
