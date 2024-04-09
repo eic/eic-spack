18639a85d9461ac15434f5568ecbf405a5ce2937
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2024-04-08 20:13:17 -0400

    chore: shift onnxruntime roll-out to EICrecon 1.13 (#620)

--- packages/eicrecon/package.py
+++ packages/eicrecon/package.py
@@ -235,7 +235,7 @@ class Eicrecon(CMakePackage):
     depends_on("catch2", when="@1.0.0:")
     depends_on("cppgsl", when="@1.7:")
     depends_on("algorithms", when="@1.7:")
-    depends_on("py-onnxruntime", when="@1.11:") # FIXME change to 1.12 when released
+    depends_on("py-onnxruntime", when="@1.12:") # FIXME change to 1.13 when released
 
     def setup_run_environment(self, env):
         env.prepend_path(
