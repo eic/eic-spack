96c6dbf1815664f75d91432265a89a6908f3d3f6
Wouter Deconinck <wdconinc@gmail.com>
2024-05-21 11:57:38 -0500

    epic: don't set JUGGLER_* and DETECTOR_VERSION variables

--- packages/epic/package.py
+++ packages/epic/package.py
@@ -173,11 +173,6 @@ class Epic(CMakePackage):
 
     def setup_run_environment(self, env):
         env.prepend_path("LD_LIBRARY_PATH", self.prefix.lib)
-        env.set("JUGGLER_DETECTOR_PATH", join_path(self.prefix.share, "epic"))
-        env.set("JUGGLER_DETECTOR", "epic")
-        env.set("JUGGLER_DETECTOR_CONFIG", "epic")
-        env.set("JUGGLER_DETECTOR_VERSION", str(self.spec.version))
         env.set("DETECTOR_PATH", join_path(self.prefix.share, "epic"))
         env.set("DETECTOR", "epic")
         env.set("DETECTOR_CONFIG", "epic")
-        env.set("DETECTOR_VERSION", str(self.spec.version))
