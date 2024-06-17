e03363bf3e137948c010d7a5256e2ed97a165ec7
Kolja Kauder <kkauder@gmail.com>
2024-06-17 14:16:33 -0400

    More tags for hepmc

--- packages/hepmcmerger/package.py
+++ packages/hepmcmerger/package.py
@@ -28,7 +28,7 @@ class Hepmcmerger(Package):
 
     # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://github.com/eic/HEPMC_Merger
-    url = "https://github.com/eic/HEPMC_Merger/releases/tag/v1.0.3"
+    url = "https://github.com/eic/HEPMC_Merger/releases/tag/v1.0.4"
     list_url = "https://github.com/eic/HEPMC_Merger/tags"
     git = "https://github.com/eic/HEPMC_Merger"
 
@@ -37,6 +37,7 @@ class Hepmcmerger(Package):
     tags = ["eic"]
 
     version("main", branch="main")
+    version("v1.0.4", sha256="2255363aecdf53ad926002656dd404319b77d287127b82894aa54c91ae482688")
     version("v1.0.3", sha256="d09c3ce65cc41a5f0bed4111deaa2d0d0e37173725c4caf873fd8f312f37096e")
     version("v1.0.2", sha256="50d8be2b69cf87460c69bf261cf61c64587ea5aadcfe344aec1b0a1d8b59b386")
     version("v1.0.1", sha256="f244ff4de311c164597eaedf547a7305b6d3cc1e6b2bc6e6d9acb8e603a44a4b")
