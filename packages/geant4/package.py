143c55546ea721f2d63f9468afd54a5d9cc3c051
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2024-06-22 08:10:50 -0400

    geant4: fix commands in /particle/property/decay/

--- packages/geant4/package.py
+++ packages/geant4/package.py
@@ -9,3 +9,7 @@ class Geant4(BuiltinGeant4):
 
     ## Add to the hardcoded GEANT4 Birk's constants
     patch("birks.patch", when="@10.7.0:")
+
+    ## Fix commands in /particle/property/decay/
+    patch("https://github.com/Geant4/geant4/commit/e32bc92498d87fb42829b08348d7fad89bc89404.diff",
+          sha256="0581bd4869ab1a7459b639b63a041d01b672247e701f1106580bfce9209c47ed")
