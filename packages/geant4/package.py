f2f171a22c997a960ec07462cc89afbe3e06a53e
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2024-06-22 08:20:47 -0400

    fix incorrect merge conflict resolution

--- packages/geant4/package.py
+++ packages/geant4/package.py
@@ -10,11 +10,8 @@ class Geant4(BuiltinGeant4):
     version("11.1.2.east", git="https://github.com/eic/geant4", branch="east-v11.1.2")
     version("11.1.1.east", git="https://github.com/eic/geant4", branch="east-v11.1.1")
 
-    ## Add to the hardcoded GEANT4 Birk's constants
-    patch("birks.patch", when="@10.7.0:")
+    patch("G4LogicalSkinSurface.patch", when="@11.2")
 
     ## Fix commands in /particle/property/decay/
     patch("https://github.com/Geant4/geant4/commit/e32bc92498d87fb42829b08348d7fad89bc89404.diff",
           sha256="0581bd4869ab1a7459b639b63a041d01b672247e701f1106580bfce9209c47ed")
-
-    patch("G4LogicalSkinSurface.patch", when="@11.2")
