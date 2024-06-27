c3f5949f568c1b74697196446a791b8323fb92db
Wouter Deconinck <wdconinc@gmail.com>
2024-06-27 14:26:11 -0500

    acts: add podio schema_version bugfix

--- packages/acts/package.py
+++ packages/acts/package.py
@@ -4,11 +4,17 @@ from spack.pkg.builtin.acts import Acts as BuiltinActs
 
 class Acts(BuiltinActs):
     patch(
+        "https://github.com/acts-project/acts/pull/3132.patch?full_index=1",
+        sha256="e0c97940abc2b4eab50834f76dcfcf2e651b7bf961ad3dd6e124a56d4d5e1779",
+        when="@30.3.0:34",
+    )
+    patch(
         "https://github.com/acts-project/acts/commit/3255dfc3dddf9c7a82aaddb041d4a6f095d19124.patch?full_index=1",
         sha256="60317f6a09a7d57721c1234fcf087ae85aeab27653976d1d3ac7a846c3b85a89",
         when="@20.1.0:26",
     )
     def cmake_args(self):
         args = super().cmake_args()
-        args.append(self.define("Python_EXECUTABLE", self.spec["python"].command.path))
+        if self.spec.satisfied("^python"):
+            args.append(self.define("Python_EXECUTABLE", self.spec["python"].command.path))
         return args
