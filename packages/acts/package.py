01602726649f5f1859b48fefde6b7fa883961daf
Wouter Deconinck <wdconinc@gmail.com>
2024-06-29 11:49:55 -0500

    acts: don't apply schema_version patch to v34.1

--- packages/acts/package.py
+++ packages/acts/package.py
@@ -6,7 +6,7 @@ class Acts(BuiltinActs):
     patch(
         "https://github.com/acts-project/acts/pull/3132.patch?full_index=1",
         sha256="e0c97940abc2b4eab50834f76dcfcf2e651b7bf961ad3dd6e124a56d4d5e1779",
-        when="@30.3.0:34",
+        when="@30.3.0:34:0",
     )
     patch(
         "https://github.com/acts-project/acts/commit/3255dfc3dddf9c7a82aaddb041d4a6f095d19124.patch?full_index=1",
