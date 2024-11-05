9b54ce75cffb3cf3329cf20b228bdb6a2f5aa3fe
Wouter Deconinck <wdconinc@gmail.com>
2024-11-05 14:21:31 -0600

    hepmcmerger: fix checksums

--- packages/hepmcmerger/package.py
+++ packages/hepmcmerger/package.py
@@ -20,11 +20,11 @@ class Hepmcmerger(CMakePackage):
     tags = ["eic"]
 
     version("main", branch="main")
-    version("1.0.4", sha256="2255363aecdf53ad926002656dd404319b77d287127b82894aa54c91ae482688")
-    version("1.0.3", sha256="d09c3ce65cc41a5f0bed4111deaa2d0d0e37173725c4caf873fd8f312f37096e")
-    version("1.0.2", sha256="50d8be2b69cf87460c69bf261cf61c64587ea5aadcfe344aec1b0a1d8b59b386")
-    version("1.0.1", sha256="f244ff4de311c164597eaedf547a7305b6d3cc1e6b2bc6e6d9acb8e603a44a4b")
-    version("1.0.0", sha256="c6088ba6ce73a92d165bc82e19f6670a1545fcfb6f352cdf5629c395a95c16af")
+    version("1.0.4", sha256="0f5e1d6b2d76af764f5cc528ccfad1269047f6e361b6bc0a1b80941388f71437")
+    version("1.0.3", sha256="9f245dc46e159f9424383a4337ebbc685973e83240fe178c776e6cca5e9674f0")
+    version("1.0.2", sha256="e8bb639545e472f46b7de8f0c6e03c9ee61086c92b8ffcba661f0fe3b1064ad6")
+    version("1.0.1", sha256="419732c2d46afbad89e32362d339a643dc1e6e5ff9724c3027a45aef1b8fbf95")
+    version("1.0.0", sha256="5f36b0b65f1062aab79dc6653b6f6fecb9682022f1a471efa62b5614c9731618")
 
     depends_on("hepmc3")
     depends_on("root")
