d1ec0d76ab3cd8d07480e41ebd6632c071c670d3
Wouter Deconinck <wdconinc@gmail.com>
2024-11-05 14:16:21 -0600

    hepmcmerger: canonicalize versions, git url

--- packages/hepmcmerger/package.py
+++ packages/hepmcmerger/package.py
@@ -13,18 +13,18 @@ class Hepmcmerger(CMakePackage):
     homepage = "https://github.com/eic/HEPMC_Merger"
     url = "https://github.com/eic/HEPMC_Merger/archive/refs/tags/v1.0.4.tar.gz"
     list_url = "https://github.com/eic/HEPMC_Merger/tags"
-    git = "https://github.com/eic/HEPMC_Merger"
+    git = "https://github.com/eic/HEPMC_Merger.git"
 
     maintainers("kkauder")
 
     tags = ["eic"]
 
     version("main", branch="main")
-    version("v1.0.4", sha256="2255363aecdf53ad926002656dd404319b77d287127b82894aa54c91ae482688")
-    version("v1.0.3", sha256="d09c3ce65cc41a5f0bed4111deaa2d0d0e37173725c4caf873fd8f312f37096e")
-    version("v1.0.2", sha256="50d8be2b69cf87460c69bf261cf61c64587ea5aadcfe344aec1b0a1d8b59b386")
-    version("v1.0.1", sha256="f244ff4de311c164597eaedf547a7305b6d3cc1e6b2bc6e6d9acb8e603a44a4b")
-    version("v1.0.0", sha256="c6088ba6ce73a92d165bc82e19f6670a1545fcfb6f352cdf5629c395a95c16af")
+    version("1.0.4", sha256="2255363aecdf53ad926002656dd404319b77d287127b82894aa54c91ae482688")
+    version("1.0.3", sha256="d09c3ce65cc41a5f0bed4111deaa2d0d0e37173725c4caf873fd8f312f37096e")
+    version("1.0.2", sha256="50d8be2b69cf87460c69bf261cf61c64587ea5aadcfe344aec1b0a1d8b59b386")
+    version("1.0.1", sha256="f244ff4de311c164597eaedf547a7305b6d3cc1e6b2bc6e6d9acb8e603a44a4b")
+    version("1.0.0", sha256="c6088ba6ce73a92d165bc82e19f6670a1545fcfb6f352cdf5629c395a95c16af")
 
     depends_on("hepmc3")
     depends_on("root")
