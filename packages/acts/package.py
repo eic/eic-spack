c2e3658a5433d833d0b5da980683153dc1ca46a1
Wouter Deconinck <wdconinc@gmail.com>
2024-11-21 20:08:32 -0600

    acts: remove podio@:0 version limit in upstream spack

--- packages/acts/package.py
+++ packages/acts/package.py
@@ -3,12 +3,28 @@ from spack.pkg.builtin.acts import Acts as BuiltinActs
 
 
 class Acts(BuiltinActs):
+    def __init__(self, spec):
+        super(Acts, self).__init__(spec)
+        # HACK Remove upstream limitations on podio@:0
+        for _spec in ["@:35+edm4hep", "@:35+podio"]:
+            if spack.spec.Spec(_spec) in Acts.dependencies:
+                del Acts.dependencies[spack.spec.Spec(_spec)]
+
     patch(
         "https://github.com/acts-project/acts/commit/3255dfc3dddf9c7a82aaddb041d4a6f095d19124.patch?full_index=1",
         sha256="60317f6a09a7d57721c1234fcf087ae85aeab27653976d1d3ac7a846c3b85a89",
         when="@20.1.0:26",
     )
+
+    @when("@34:35")
+    def patch(self):
+        super().patch()
+        # HACK Remove upstream limitations on podio@:0
+        filter_file("_acts_podio_version 0.16", "_acts_podio_version 1.0", "CMakeLists.txt")
+
     def cmake_args(self):
         args = super().cmake_args()
         args.append(self.define("Python_EXECUTABLE", self.spec["python"].command.path))
         return args
+
+_acts = Acts("acts")
