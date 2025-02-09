bb2e735ff72908068246f368c7ade9757db0fb42
Wouter Deconinck <wdconinc@gmail.com>
2025-02-09 12:09:39 -0600

    k4actstracking: filter_file when Acts version >= 37

---
+++ packages/k4actstracking/package.py
@@ -0,0 +1,12 @@
+from spack import *
+from spack.pkg.k4.k4actstracking import K4actstracking as BuiltinK4actstracking
+
+
+class K4actstracking(BuiltinK4actstracking):
+    def patch(self):
+        filter_file(
+            "m_obj.write(m_outputFileName)",
+            "m_obj.write(m_outputFileName.value())",
+            "k4ActsTracking/src/components/ActsGeoSvc.cpp",
+            string=True,
+        )
