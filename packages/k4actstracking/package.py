a9bd632753eb1b52fa730441f50b07cd02a74d66
Wouter Deconinck <wdconinc@gmail.com>
2025-02-12 13:39:18 -0600

    k4actstracking: allow package repo to work without key4hep-spack

--- packages/k4actstracking/package.py
+++ packages/k4actstracking/package.py
@@ -1,12 +1,16 @@
 from spack.package import *
-from spack.pkg.k4.k4actstracking import K4actstracking as BuiltinK4actstracking
 
+try:
+    from spack.pkg.k4.k4actstracking import K4actstracking as BuiltinK4actstracking
 
-class K4actstracking(BuiltinK4actstracking):
-    def patch(self):
-        filter_file(
-            "m_obj.write(m_outputFileName)",
-            "m_obj.write(m_outputFileName.value())",
-            "k4ActsTracking/src/components/ActsGeoSvc.cpp",
-            string=True,
-        )
+    class K4actstracking(BuiltinK4actstracking):
+        def patch(self):
+            filter_file(
+                "m_obj.write(m_outputFileName)",
+                "m_obj.write(m_outputFileName.value())",
+                "k4ActsTracking/src/components/ActsGeoSvc.cpp",
+                string=True,
+            )
+
+except:
+    pass
