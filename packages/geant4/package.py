6e419ae447f367df4a570278ecfc18d72f5c4cf2
Wouter Deconinck <wdconinc@gmail.com>
2025-03-27 16:18:19 -0500

    geant4: add v11.3.1.east (#726)
    ### Briefly, what does this PR introduce?
    This PR adds `geant4`, v11.3.1.east.

--- packages/geant4/package.py
+++ packages/geant4/package.py
@@ -4,6 +4,7 @@ from spack.pkg.builtin.geant4 import Geant4 as BuiltinGeant4
 
 class Geant4(BuiltinGeant4):
     ## Versions with the eAST physics list
+    version("11.3.1.east", git="https://github.com/eic/geant4", branch="east-v11.3.1")
     version("11.3.0.east", git="https://github.com/eic/geant4", branch="east-v11.3.0")
     version("11.2.2.east", git="https://github.com/eic/geant4", branch="east-v11.2.2")
     version("11.2.1.east", git="https://github.com/eic/geant4", branch="east-v11.2.1")
