11608ef2b03a168a461b2279b1bb85b6a05f5fbe
Wouter Deconinck <wdconinc@gmail.com>
2025-05-26 14:36:24 -0500

    root: fix typo

--- packages/root/package.py
+++ packages/root/package.py
@@ -8,6 +8,7 @@ class Root(BuiltinRoot):
         "https://github.com/root-project/root/pull/18830.patch?full_index=1",
         sha256="deced0c6c7ad8be44757366f8e2b222f0e1a19dc4deed78d07d5a4a672d40d62",
         when="@6.32:6.32.12",
+    )
     # Apply TFile::k630forwardCompatibility when creating new file if set in rootrc
     patch(
         "https://github.com/root-project/root/pull/17542.patch?full_index=1",
