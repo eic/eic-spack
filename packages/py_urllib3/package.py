19b7e644e94a985527bac4aa2d8a759e329e17b8
Sakib Rahman <rahmans@myumanitoba.ca>
2025-06-16 12:05:25 -0400

    Update py urllib3 as a dependency for rucio client

---
+++ packages/py_urllib3/package.py
@@ -0,0 +1,9 @@
+from spack.package import *
+from spack.pkg.builtin.py_urllib3 import PyUrllib3 as PyUrllib3
+
+class PyUrllib3(BuiltinPyUrllib3):
+    version("2.3.0", sha256="f8c5449b3cf0861679ce7e0503c7b44b5ec981bec0d1d3795a07f1ba96f0204d")
+
+    depends_on("python@3.8:", when="@2.1.0:2.2", type=("build", "run"))
+    depends_on("python@3.9:", when="@2.3:", type=("build", "run"))
+    depends_on("py-hatch-vcs@0.4.0", when="@2.3:", type="build")
