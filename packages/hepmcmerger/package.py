a155cd08eeaa9ad793340ad75dc570704f1cb6c6
Sakib Rahman <rahmans@myumanitoba.ca>
2025-06-19 18:19:29 -0400

    Add hepmcmerger version 2.0.0 (#757)
    ### Briefly, what does this PR introduce?
    Capability to add infinite number of background files in the merger
    
    
    ### What kind of change does this PR introduce?
    - [ ] Bug fix (issue #__)
    - [ ] New feature (issue #__)
    - [ ] Documentation update
    - [ ] Other: __
    
    ### Please check if this PR fulfills the following:
    - [ ] Tests for the changes have been added
    - [ ] Documentation has been added / updated
    - [ ] Changes have been communicated to collaborators
    
    ### Does this PR introduce breaking changes? What changes might users
    need to make to their code?
    
    ### Does this PR change default behavior?

--- packages/hepmcmerger/package.py
+++ packages/hepmcmerger/package.py
@@ -20,6 +20,7 @@ class Hepmcmerger(CMakePackage):
     tags = ["eic"]
 
     version("main", branch="main")
+    version("2.0.0", sha256="901dd224aa68c308fc34fc3b859ef9e04cacfd915e5c11cf3a98ad1ec372b5ce")
     version("1.1.1", sha256="c55f2016901feb081c87ad275b99a4e9da0ce476a23c0c1fa15a347ca1018f97")
     version("1.1.0", sha256="69385f36f1d4d9b9c725afc5b40dbf0ba4253e3fccde76f989d84afee72d36b6")
     version("1.0.5", sha256="3660c602212368f04e98a36ced68e4ea3dcc8a23a0cb5047ef27afe07c16bf32")
