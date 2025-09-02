76559047cd8ff329f4a85f09f5aedcd5084ccfcd
Wouter Deconinck <wdconinc@gmail.com>
2025-09-02 16:31:44 -0500

    py-tensorflow: patch for clang + cuda (#776)
    ### Briefly, what does this PR introduce?
    This PR adds a patch to `py-tensorflow` such that it works properly on
    recent clang + cuda.
    
    ### What kind of change does this PR introduce?
    - [x] Bug fix (issue:
    https://eicweb.phy.anl.gov/containers/eic_container/-/jobs/5915382#L3645)
    - [ ] New feature (issue #__)
    - [ ] Documentation update
    - [ ] Other: __
    
    ### Please check if this PR fulfills the following:
    - [ ] Tests for the changes have been added
    - [ ] Documentation has been added / updated
    - [ ] Changes have been communicated to collaborators
    
    ### Does this PR introduce breaking changes? What changes might users
    need to make to their code?
    No.
    
    ### Does this PR change default behavior?
    No.

---
+++ packages/py-tensorflow/package.py
@@ -0,0 +1,13 @@
+from spack.package import *
+
+try:
+    from spack_repo.builtin.packages.py_tensorflow.package import PyTensorflow as BuiltinPyTensorflow
+except:
+    from spack.pkg.builtin.py_tensorflow import PyTensorflow as BuiltinPyTensorflow
+
+class PyTensorflow(BuiltinPyTensorflow):
+    patch(
+        "https://github.com/tensorflow/tensorflow/pull/90579.diff?full_index=1",
+        sha256="f623d5d833ba0185c9b6ef4a98b90069a73bea84e45381995a3db8150280c896",
+        when="@2.15:2.19",
+    )
