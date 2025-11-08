d02065e8ed87e720a67449c8aef591d88f31ffd6
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2025-11-08 09:13:31 -0500

    eicrecon: add 1.31.0

--- packages/eicrecon/package.py
+++ packages/eicrecon/package.py
@@ -19,6 +19,7 @@ class Eicrecon(CMakePackage):
     maintainers = ["wdconinc"]
 
     version("main", branch="main")
+    version("1.31.0", sha256="d417ce403bb631d20b14d9ea65ac9203a6d049bb234f631a8912eeebf679ea65")
     version("1.30.2", sha256="127a05a448b777aca9f73e047ac8cca054a46f59821743d2e39c2c8ea4e18533")
     version("1.30.1", sha256="2cdbe2b20a26e35f7db15ab003fa99fdb7b16f14037381ba8c77bfe34b849bba")
     version("1.30.0", sha256="72b7af4521d83f454650a096b53936e795ca8292f7268100d392a217ec84e809")
