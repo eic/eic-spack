bc8e0a2111547c40287533a8d47c53efca98382e
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2025-11-10 07:28:50 -0500

    epic: add 25.11.1

--- packages/epic/package.py
+++ packages/epic/package.py
@@ -15,6 +15,7 @@ class Epic(CMakePackage):
     tags = ["eic"]
 
     version("main", branch="main")
+    version("25.11.1", sha256="073a0dfde0aa7f26a80a8a320456c14a83ba0f23da7387b02c0db936217f89ba")
     version("25.11.0", sha256="ed1e5f29589f197d342ec5287d3bda1502239f3b52a535a5cd2de457034698d8")
     version("25.10.3", sha256="5fcb3a7e83b37f507a62365eb33f74997a521244c622bf54fe9dc405f9c6dcf9")
     version("25.10.2", sha256="c97fb37646e8083f97b4ebcc0c73e97febbbec43e9aeaf9711fab7d515862696")
