8f941e50aa5c3f3e9646714e8f4a3408982d577e
Wouter Deconinck <wdconinc@gmail.com>
2026-02-22 17:15:53 -0600

    jana2: remove unsatisfiable versions (podio too old)

--- spack_repo/eic/packages/jana2/package.py
+++ spack_repo/eic/packages/jana2/package.py
@@ -36,24 +36,8 @@ class Jana2(CMakePackage, CudaPackage):
     version("2.3.0", sha256="7ebc914f1dafbd50d7296c92d67658b64f80e309e9812653413b525d84320b1d")
     version("2.2.1-rc1", sha256="7b65ce967d9c0690e22f4450733ead4acebf8fa510f792e0e4a6def14fb739b1")
     version("2.2.0", sha256="60940e182593dafddaa76d582d3270ac47694fa3f20257493e1017b34f624ba9")
-    version("2.1.2", sha256="81b5b323289113d0fe666927afa3f81e76b3fcee0df7bff2c3fc1cb72fb240ce")
-    version("2.1.1", sha256="ea9926e0fb2fa1fa1f22acb6411d5bd521733f25031df114f118fa7fe2272a14")
-    version("2.1.0", sha256="111f7a3c3a2357a4bbf54370740b22f641a99c83ec649d4ea9899c143371cf35")
-    version(
-        "2.0.9",
-        sha256="d8df3dc3390a239eae64eb58f6a5745608405b8aa91fb247965aaf2e321d269b",
-        deprecated=True,
-    )
-    version("2.0.8", sha256="b0c91a2780a66cd51cfeaf2ddbe44e2264afb4b2b2a91bc2d001802fb40c3a1b")
-    version("2.0.7", sha256="56b4d1858d0f84e655b903eef07f5c6e23a9ed62219cd083ae279549dd051eb1")
-    version("2.0.6", sha256="dc0bec6a63b6973171a714cb9eb2044b96c28f7aa5dd198e5be5858e5ec7ce7c")
-    version("2.0.5", sha256="2e7297dfb0bd7f4a2f2fa3bca6b1c10b2553d321dec6060e48b0d75a5ed6717d")
-    version("2.0.4", sha256="848adffcb881beb7835d01ce47a58991bb4f92664c9477196960ce8cfd94a3ca")
-    version("2.0.3", sha256="fd34c40e2d6660ec08aca9208999dd9c8fe17de21c144ac68b6211070463e415")
-    version("2.0.2", sha256="161d29c2b1efbfb36ec783734b45dff178b0c6bd77a2044d5a8829ba5b389b14")
-    version("2.0.1", sha256="1471cc9c3f396dc242f8bd5b9c8828b68c3c0b72dbd7f0cfb52a95e7e9a8cf31")
 
-    variant("podio", default=False, description="Build with PODIO support.", when="@2.1.0:")
+    variant("podio", default=False, description="Build with PODIO support.")
     variant("python", default=True, description="Build with Python bindings.")
     variant("root", default=False, description="Use ROOT for janarate.")
     variant("xerces", default=True, description="Build with XML support.")
@@ -70,30 +54,9 @@ class Jana2(CMakePackage, CudaPackage):
     with when("+podio"):
         depends_on("podio@0.16.3:")
         depends_on("podio@:1.4", when="@:2.4.2")  # uses operator-> on collections
-        depends_on("podio@:0.17.3", when="@:2.1.2")  # uses podio/EventStore.h
         depends_on("py-jinja2")
         depends_on("py-pyyaml")
 
-    conflicts("+cuda", when="@:2.0", msg="CUDA support only available in 2.1 and later")
-
-    # JFactoryPodioT.h: ClearData (https://github.com/JeffersonLab/JANA2/pull/243)
-    patch(
-        "https://github.com/JeffersonLab/JANA2/pull/243.patch?full_index=1",
-        sha256="f1b89fe245ac44fec815b7cf97d40924acf1281cd2aeb31ee33ad76a13e0abd4",
-        when="@2.1.1",
-    )
-    # JResourcePool.h: alignas (https://github.com/JeffersonLab/JANA2/pull/239)
-    patch(
-        "https://github.com/JeffersonLab/JANA2/pull/239.patch?full_index=1",
-        sha256="a32220ba30f18f30c196a604c9ccf9ed01676a81aadfad0028214b42a4363e25",
-        when="@2.0.6:2.1.1",
-    )
-    # JBacktrace.h: free after use (https://github.com/JeffersonLab/JANA2/pull/224)
-    patch(
-        "https://github.com/JeffersonLab/JANA2/pull/224.patch?full_index=1",
-        sha256="cc3820e391caae8f4fe08d96f49fa2b38e454aaf37edde7dbd6fb4537a01af61",
-        when="@2.0.2:2.1.0",
-    )
     # Stop printing the component summary
     patch(
         "https://github.com/JeffersonLab/JANA2/commit/8ed069da7f307d12cafd6b075eae8401aec6f5aa.diff?full_index=1",
