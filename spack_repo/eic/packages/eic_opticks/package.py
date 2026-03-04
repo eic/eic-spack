f9b9adfa778645d6aa5f53d72d32d9c80d11d8e6
Dmitri Smirnov <dmixsmi@gmail.com>
2026-03-04 11:21:28 -0500

    eic_opticks: add version 0.1.0
    Add version 0.1.0 with corresponding GitHub archive URL and checksum.

--- spack_repo/eic/packages/eic_opticks/package.py
+++ spack_repo/eic/packages/eic_opticks/package.py
@@ -12,12 +12,14 @@ class EicOpticks(CMakePackage, CudaPackage):
 
     homepage = "https://github.com/bnlnpps/eic-opticks"
     git = "https://github.com/bnlnpps/eic-opticks.git"
+    url = "https://github.com/BNLNPPS/eic-opticks/archive/refs/tags/0.1.0.tar.gz"
 
     license("Apache-2.0")
 
     maintainers("plexoos")
 
     version("main", branch="main")
+    version("0.1.0", sha256="b9b42254d3a2c57df9502e2920c7078aee3b7952d4de1d0299fd421d88a5950d")
 
     depends_on("cxx", type="build")
     depends_on("cmake@3.10:", type="build")
