3d29d1a016eb09ca0e695ab994fd4d7ccd5d4c71
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-03-11 21:31:48 -0400

    hepmcmerger: add 2.2.0

--- spack_repo/eic/packages/hepmcmerger/package.py
+++ spack_repo/eic/packages/hepmcmerger/package.py
@@ -21,6 +21,7 @@ class Hepmcmerger(CMakePackage):
     tags = ["eic"]
 
     version("main", branch="main")
+    version("2.2.0", sha256="ae513c0653afed35c1ec1cb4e209b0dd990b7c658732829168404475bac41c8c")
     version("2.1.0", sha256="fbff886e503fd7ccd1258857b69c9bcc4a8ee55dd3d9c345b53416d9a9708c6e")
     version("2.0.0", sha256="901dd224aa68c308fc34fc3b859ef9e04cacfd915e5c11cf3a98ad1ec372b5ce")
     version("1.1.1", sha256="c55f2016901feb081c87ad275b99a4e9da0ce476a23c0c1fa15a347ca1018f97")
