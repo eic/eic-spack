20437d6e9fdf137f2f2050311d97c82c31bf7e4d
Wouter Deconinck <wdconinc@gmail.com>
2026-04-13 12:50:59 -0500

    g4occt: support dd4hep plugin

--- spack_repo/eic/packages/g4occt/package.py
+++ spack_repo/eic/packages/g4occt/package.py
@@ -18,13 +18,16 @@ class G4occt(CMakePackage):
 
     variant("tests", default=False, description="Build test suite")
     variant("benchmarks", default=False, description="Build benchmark suite")
+    variant("dd4hep", default=False, description="Build DD4hep plugin")
 
     depends_on("cmake@3.16:", type="build")
     depends_on("geant4@11.3:")
     depends_on("opencascade@7.8:")
+    depends_on("dd4hep", when="+dd4hep")
 
     def cmake_args(self):
         return [
             self.define_from_variant("BUILD_TESTING", "tests"),
             self.define_from_variant("BUILD_BENCHMARKS", "benchmarks"),
+            self.define_from_variant("BUILD_DD4HEP_PLUGIN", "dd4hep"),
         ]
