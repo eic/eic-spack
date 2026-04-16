90ba4cf470a2a7ddfcec42c18b63930a0e4f08b8
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-04-16 11:01:09 -0400

    Revert "epic: remove setup_run_environment (#856)"
    This reverts commit 7bf4922f1895ff56dbae6567f73c0da57fceffe4.

--- spack_repo/eic/packages/epic/package.py
+++ spack_repo/eic/packages/epic/package.py
@@ -221,3 +221,9 @@ class Epic(CMakePackage):
                 os.environ["DETECTOR_PATH"] = detector_path
                 checkGeometry = Executable(join_path(spec['dd4hep'].prefix.bin, 'checkGeometry'))
                 checkGeometry('-c', join_path(detector_path, spec.variants["artifacts"].value + ".xml"))
+
+    def setup_run_environment(self, env):
+        env.prepend_path("LD_LIBRARY_PATH", self.prefix.lib)
+        env.set("DETECTOR_PATH", join_path(self.prefix.share, "epic"))
+        env.set("DETECTOR", "epic")
+        env.set("DETECTOR_CONFIG", "epic")
