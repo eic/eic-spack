538b58d7136d643f6d8bc199b10c42d600533429
Wouter Deconinck <wdconinc@gmail.com>
2026-04-20 21:39:44 -0500

    eicrecon: depends on root +tmva when @1.14:

--- spack_repo/eic/packages/eicrecon/package.py
+++ spack_repo/eic/packages/eicrecon/package.py
@@ -75,6 +75,7 @@ class Eicrecon(CMakePackage):
     depends_on("acts@30:")
 
     depends_on("root")
+    depends_on("root +tmva", when="@1.14:")
     depends_on("fastjet")
     depends_on("fjcontrib")
     depends_on("fmt")
