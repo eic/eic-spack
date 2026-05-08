3c3a36cbf9aadc6cdc891b62dedcbe1ae8bfb6de
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-05-08 07:15:10 -0400

    epic: add 26.05.0

--- spack_repo/eic/packages/epic/package.py
+++ spack_repo/eic/packages/epic/package.py
@@ -18,6 +18,7 @@ class Epic(CMakePackage):
     tags = ["eic"]
 
     version("main", branch="main")
+    version("26.05.0", sha256="d8b31ab028caa872b84194c70e5dfeee2053cdae3905f5be14597f2368cf089c")
     version("26.04.1", sha256="c75fc72ef0abf5c88ac47470a69ef9127774ac18bf1ab984790285524b26f5eb")
     version("26.04.0", sha256="4512787f35ed4bb5433ed92893926673c3847cd3f94534fa626ad5977fd15e12")
     version("26.03.1", sha256="06dddba9f0e0ed872172d5f612974fb4ef2e40392ec8cac6267bf332275e067b")
