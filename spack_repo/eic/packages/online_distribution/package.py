eb5238b4968cb6fed534d8ef9f80af5789799354
Wouter Deconinck <wdconinc@gmail.com>
2026-05-12 19:29:03 -0500

    Update make command to include CPPFLAGS

--- spack_repo/eic/packages/online_distribution/package.py
+++ spack_repo/eic/packages/online_distribution/package.py
@@ -44,7 +44,7 @@ class OnlineDistribution(Package):
                 autoreconf("-fvi")
                 configure = Executable("./configure")
                 configure(f"--prefix={prefix}")
-                make("HAVE_GETOPT_H=1")
+                make("CPPFLAGS=-DHAVE_GETOPT_H=1")
                 make("install")
 
     def setup_run_environment(self, env):
