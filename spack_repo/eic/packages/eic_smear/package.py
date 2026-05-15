7f7ccfad464ad6449a400171e131994c906e8246
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-05-14 21:15:09 -0400

    eic_smear: add 1.1.17 (#924)
    https://github.com/eic/eic-smear/releases/tag/1.1.17

--- spack_repo/eic/packages/eic_smear/package.py
+++ spack_repo/eic/packages/eic_smear/package.py
@@ -24,6 +24,7 @@ class EicSmear(CMakePackage):
     variant("pythia6", default=False, description="Include Pythia6 support")
 
     version("master", branch="master")
+    version("1.1.17", sha256="1a9aa4079626346e45d4e04cc5c577ad09b01544d6c7d3e219d7bb3e18751808")
     version("1.1.16", sha256="5deda2adb70004bc9fb59dc3cd99cc656c370b663c0cdedbc02497af774272ca")
     version("1.1.15", sha256="d50b25a1dbfb9d1f169a12ae95bbb73088d46fcde2a9287360288de108c78f6a")
     version("1.1.14", sha256="d5acd5c13bf01b0eac4274caec779b0de2af2e552cdae751a6d0a772068c48cb")
