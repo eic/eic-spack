6eb9a34978ed318acd7531a6627e1f11fa1648a9
Wouter Deconinck <wdconinc@gmail.com>
2026-06-03 07:47:47 -0500

    root: patch: [geom] Fix tessellated closure checks after initialization

--- spack_repo/eic/packages/root/package.py
+++ spack_repo/eic/packages/root/package.py
@@ -15,7 +15,7 @@ class Root(BuiltinRoot):
 
     # [geom] Fix tessellated closure checks after initialization
     patch(
-        "https://github.com/root-project/root/commit/d4435369043bbd45a30c10a3ee25e1d4df316b5f.patch?full_index=1",
+        "https://github.com/root-project/root/pull/22457.patch?full_index=1",
         sha256="67b953213449b57df76ccce374dad4d4e9f3b5158f6c5241c974cd4f0f6b0d47",
         when="@6.40.00",
     # [python] Ensure GIL is held during CPython API call
