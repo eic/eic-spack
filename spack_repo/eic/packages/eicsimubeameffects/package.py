77aec499d70c81083905930aff7aef5b2c7044d3
Wouter Deconinck <wdconinc@gmail.com>
2026-07-22 10:54:57 -0500

    eicsimubeameffects: depends_on c

--- spack_repo/eic/packages/eicsimubeameffects/package.py
+++ spack_repo/eic/packages/eicsimubeameffects/package.py
@@ -23,6 +23,7 @@ class Eicsimubeameffects(CMakePackage):
     version("1.1", sha256="b71279829d88f690118725bbfcf378fccb6d0f79ba8a595932617a3c1e8b79aa")
     version("1.0", sha256="f981f84be6fe9bf6ae7420fecc16e5c7ac6905ad9f18c789f5c0ef1beec0cb46")
 
+    depends_on("c", type="build")
     depends_on("cxx", type="build")
     depends_on("cmake@3.20:", type="build")
 
