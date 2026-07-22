146fe585fee06b177fd0c7d514b6c714245fae2f
Dmitry Kalinkin <dmitry.kalinkin@gmail.com>
2026-07-22 11:13:38 -0400

    eicsimubeameffects: bump to 1.0, remove old tags

--- spack_repo/eic/packages/eicsimubeameffects/package.py
+++ spack_repo/eic/packages/eicsimubeameffects/package.py
@@ -11,9 +11,7 @@ class Eicsimubeameffects(CMakePackage):
     """EIC beam effects simulation studies and utilities."""
 
     homepage = "https://github.com/eic/eicSimuBeamEffects"
-    url = (
-        "https://github.com/eic/eicSimuBeamEffects/archive/refs/tags/production_2021-09-27.tar.gz"
-    )
+    url = "https://github.com/eic/eicSimuBeamEffects/archive/refs/tags/v1.0.tar.gz"
     list_url = "https://github.com/eic/eicSimuBeamEffects/tags"
     git = "https://github.com/eic/eicSimuBeamEffects.git"
 
@@ -22,17 +20,7 @@ class Eicsimubeameffects(CMakePackage):
     tags = ["eic"]
 
     version("master", branch="master")
-    version(
-        "2021-09-27", sha256="445a637ecc5ad35d4d0b60a7049abf8e66c3c2d6f6778accd5094ed1e092f9be"
-    )
-    version(
-        "2021-08-06", sha256="cadcfa748dea80ec12c5388aa98e74780aaab79051743f2302d62d681eee859a"
-    )
-    version(
-        "production_2021-09-27",
-        sha256="445a637ecc5ad35d4d0b60a7049abf8e66c3c2d6f6778accd5094ed1e092f9be",
-        deprecated=True,
-    )
+    version("1.0", sha256="0000000000000000000000000000000000000000000000000000000000000000")
 
     depends_on("cxx", type="build")
     depends_on("cmake@3.20:", type="build")
