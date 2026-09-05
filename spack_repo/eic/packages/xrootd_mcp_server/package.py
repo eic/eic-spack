f786bff4c9f547fc5fba5265675f0942e5602826
wdconinc <4656391+wdconinc@users.noreply.github.com>
2026-09-05 15:48:17 +0000

    [create-pull-request] automated change

--- spack_repo/eic/packages/xrootd_mcp_server/package.py
+++ spack_repo/eic/packages/xrootd_mcp_server/package.py
@@ -21,6 +21,8 @@ class XrootdMcpServer(Package):
     license("MIT", checked_by="wdconinc")
 
     version("main", branch="main")
+    version("0.2.2", sha256="6300491ec47e5e14a13d0f97b2e81c33dfedf55ceefdc67b1f5b07c2fd940ee1")
+    version("0.2.1", sha256="59b16e8f126e17354545603c390b117f64c48c266a1cdf0c402c782eb6ec61a2")
     version("0.2.0", sha256="fee4a4ac95debe4f662b0e975ffd7edc7148798127fd3db46135b3bb1b1a06f4")
     version("0.1.0", sha256="320b2974e7e04815e76e5649ea9bf0722f6bdf04435c6ea0402e09555d3e6fb1")
 
